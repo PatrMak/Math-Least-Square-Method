e202157894458a34f150fa5765443315c9852a77
Patryk Makulski <patryk.makulski@interia.pl>
2023-10-01 16:09:36 +0200

    Added: Least Square method for 2D circle.

---
+++ Math LeastSquare Method.UnitTests/FitCircle2DTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Maths;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math_LeastSquare_Method.UnitTests
+{
+    public class FitCircle2DTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetFittedCircle_CorrectPoints_ReturnFittedCricle()
+        {
+            // Arange
+            var circle = new FitCircle2D();
+            // Act
+            var points = new List<PointsDataModel>{
+                new PointsDataModel{X=127.032,Y=127.050},
+                new PointsDataModel{X=127.043,Y=-127.047},
+                new PointsDataModel{X=-127.051,Y=-127.060},
+                new PointsDataModel{X=-127.060,Y=127.038},
+            };
+            var result = circle.GetFittedCircle(points);
+            var expected = new CircleDataModel { X = -0.0092, Y = -0.0053, Radius = 179.6725 };
+            // Assert
+
+            result.Should().Equals(expected);
+        }
+
+        [Test]
+        public void GetFittedCircle_NotRquiredPointsCount_ThrowExeption()
+        {
+            // Arange
+            var circle = new FitPlaneLeastSquares3D();
+            // Act
+            var points = new List<PointsDataModel>{
+                new PointsDataModel{X=127.032,Y=127.050,Z=15.298},
+                new PointsDataModel{X=127.043,Y=-127.047,Z=15.298},
+            };
+            // Assert
+            Assert.That(() => circle.GetPlaneAngle(points), Throws.Exception);
+        }
+    }
+    
+}
---
+++ maths/FitCircle2D.cs
@@ -0,0 +1,81 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maths
+{
+    public class FitCircle2D
+    {
+
+
+
+        public CircleDataModel GetFittedCircle(List<PointsDataModel> pointsDataModels)
+        {
+            if (pointsDataModels.Count < 4)
+                throw new Exception("Required minimun 4 points");
+
+
+            var matrixJ = GetMatrixJXY(pointsDataModels);
+            var vectorABC = GetVectorABC(matrixJ);
+
+            var result = CalculateFittedCircleVector(vectorABC);
+
+
+            return new CircleDataModel { X = result[0], Y = result[1], Radius = result[2] };
+        }
+
+
+        private Vector<double> CalculateFittedCircleVector(Vector<double> vectorABC)
+        {
+            double A = vectorABC[0];
+            double B = vectorABC[1];
+            double C = vectorABC[2];
+
+            double aParams = Math.Round(-B / (2 * A),4);
+            double bParams = Math.Round(-C / (2 * A), 4);
+            double radius = Math.Round(Math.Sqrt(4 * A + Math.Pow(B, 2) + Math.Pow(C, 2))/(2*A),4);
+
+
+            var result = Vector<double>.Build.Dense(3);
+
+            result[0] = aParams;
+            result[1] = bParams;
+            result[2] = radius;
+
+            return result;
+        }
+
+
+        private Vector<double> GetVectorABC(Matrix<double> matrixJ)
+        {
+            var vectorK = Vector<double>.Build.Dense(4, 1);
+
+            LeasSquareVectorResult vectorResult = new LeasSquareVectorResult();
+
+            return vectorResult.CalculateNormalVector(matrixJ, vectorK);
+        }
+
+        private Matrix<double> GetMatrixJXY(List<PointsDataModel> pointsDataModels)
+        {
+            int rows = pointsDataModels.Count();
+
+            double[] columnX = pointsDataModels.Select(p => p.X).ToArray();
+            double[] columnY = pointsDataModels.Select(p => p.Y).ToArray();
+            double[] columnSqueredXY = pointsDataModels.Select(p => Math.Pow(p.X, 2) + Math.Pow(p.Y, 2)).ToArray();
+
+            return AddColumnToMatrix(rows, columnSqueredXY, columnX, columnY);
+        }
+
+        private Matrix<double> AddColumnToMatrix(int rows, double[] column1, double[] column2, double[] column3)
+        {
+            var result = Matrix<double>.Build.Dense(rows, 3, 1);
+
+            result.SetColumn(0, column1);
+            result.SetColumn(1, column2);
+            result.SetColumn(2, column3);
+
+            return result;
+        }
+    }
+}
